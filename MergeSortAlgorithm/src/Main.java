9b9334231193c9e231a3be8e20a836a07204bfe0
AhmetY7 <ahmetylmz95@gmail.com>
2019-09-25 19:39:30 +0300

    mergeSort

--- MergeSortAlgorithm/src/Main.java
+++ MergeSortAlgorithm/src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void  mergeSort(int[] array, int leftIncl, int rightExcl) {
+        // the base condition
+        if (rightExcl <= leftIncl +1) {
+            return;
+        }
+        // divide: calculating the middle element
+        int middle = leftIncl + (rightExcl - leftIncl) / 2;
 
+        mergeSort(array, leftIncl, middle); // conquer: sort the left subarray
+        mergeSort(array, middle, rightExcl); // conquer: sort the right subarray
+
+        // combine : merge sorted subarrays into sorted one
+        merge(array, leftIncl, middle, rightExcl);
+    }
+
+    private static void merge(int[] array, int left, int middle, int right) {
+        int l = left; // index for the left subarray
+        int m = middle; // index for the right subarray
+        int k = 0; // index for the temp subarray
+
+        int[] temp = new int[right - left]; // temporary array for merging
+
+        while (l < middle && m < middle) {
+            if (array[l] <= array[m]) {
+                temp[k] = array[l];
+                l++;
+            } else {
+                temp[k] = array[m];
+                m++;
+            }
+            k++;
+        }
+
+        // insert all the remaining elements of the left subarray in the array
+        for(;l < middle; l++, k++) {
+            temp[k] = array[l];
+        }
+
+        // insert all the remaining elements of the right subarray in the array
+        for(;m < right; m++, k++) {
+            temp[k] = array[m];
+        }
+
+        //Copying elements from temp to array
+        System.arraycopy(temp, 0, array, left, temp.length);
+    }
+    public static void main(String[] args) {
+        int[] array1 = {30, 21, 23, 19, 28, 11, 23};
+        mergeSort(array1, 0, array1.length);
+
+        int[] array2 = {30, 20, 10, 10, 20, 10};
+        mergeSort(array2, 0, array2.length);
     }
 }
